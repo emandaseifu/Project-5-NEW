7a9e5fd71f41496c435385fb26221cb5a59a78a5
emandaseifu <115669613+emandaseifu@users.noreply.github.com>
2023-04-25 22:42:40 -0400

    Add files via upload

--- Input.java
+++ Input.java
@@ -5,7 +5,7 @@ import java.text.ParseException;
 import java.io.File;
 
 /**
- * The Input class 
+ * The Input class
  *
  * @author Emanda Seifu
  * @author Shreyas Kunaparaju
@@ -14,17 +14,26 @@ import java.io.File;
  */
 public class Input {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         try {
             if (args.length == 1) {
                 ChannelReader reader = new ChannelReader(args[0]);
                 DLList<String> channelView = reader.getChannel();
-            } else {
-                ChannelReader reader = new ChannelReader("SampleInput1_2022.csv");
+            }
+            else {
+                ChannelReader reader = new ChannelReader(
+                    "SampleInput1_2022.csv");
                 DLList<String> channelView = reader.getChannel();
             }
-        } catch (Exception e) {
-            System.out.println("An error occurred while reading the input file: ");
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        }
+        catch (ParseException e) {
+            System.out.println(
+                "An error occurred while parsing the input file: " + e
+                    .getMessage());
+        }
     }
-}
+
 }
