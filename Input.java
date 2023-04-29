fedbd5f22b827bdc63c791123bde497cc995cbbd
shreyask4 <131196846+shreyask4@users.noreply.github.com>
2023-04-29 16:07:38 -0400

    Update Input.java

--- Input.java
+++ Input.java
@@ -1,3 +1,4 @@
+
 package prj5;
 
 import java.io.FileNotFoundException;
@@ -12,18 +13,24 @@ import java.io.File;
  * @version 04.28.2023
  */
 public class Input {
-
     public static void main(String[] args) throws FileNotFoundException {
+        /**
+         * This is the main method for this project and it allows the project to run
+         */
         if (args.length == 1) {
             ChannelReader reader = new ChannelReader(args[0]);
             DLList<Influencer> channelView = reader.getInfluencers();
+            // channelView.sortbyChannel();
+
         }
         else {
             ChannelReader reader = new ChannelReader("SampleInput1_2022.csv");
             DLList<Influencer> channelView = reader.getInfluencers();
-            for(int i = 0; i < channelView.size(); i++) {
+            for (int i = 0; i < channelView.size(); i++) {
                 System.out.println(channelView.get(i));
             }
+            GUI gui = new GUI(reader);
         }
     }
+
 }
