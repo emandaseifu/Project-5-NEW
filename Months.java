a1b7c5ec91039d307b63182299d725d5b453a90a
emandaseifu <115669613+emandaseifu@users.noreply.github.com>
2023-04-29 22:24:56 -0400

    Months.java

--- Months.java
+++ Months.java
@@ -1,10 +1,9 @@
 package prj5;
 
 /**
- * This enum represents the months of the year
- * JANUARY, FEBRUARY, and MARCH
+ * Represents differt months of the year
  * Each constant represents a specific month of the year
- * and can be used to indicate the month in various contexts
+ * 
  * @author Emanda Seifu
  * @author Shreyas Kunaparaju
  * @author Arnav Sharma
@@ -12,6 +11,6 @@ package prj5;
  */
 public enum Months {
 
-JANUARY, FEBRUARY, MARCH;
-    
+    JANUARY, FEBRUARY, MARCH;
+
 }
